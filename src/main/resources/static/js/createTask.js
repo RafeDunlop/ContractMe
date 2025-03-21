4ee3a4534df6d701fc087f3fa3542e74ab9e032c
rafed <rdu46@uclive.ac.nz>
2025-03-21 19:43:08 +1300

    adjusted createTask.js to implement commmons correctly. Attempted (unsuccessfully) to induce the combobox to only show options which exist (currently shows empty clickable slots for each room already linked

--- src/main/resources/static/js/createTask.js
+++ src/main/resources/static/js/createTask.js
@@ -1,17 +1,12 @@
 import {
-    setRoomList,
-    addRoom,
     updateCharCounter,
-    checkRoomName,
     addRoomsToSubmission,
     renderRooms
 } from "./renovationCommons.js";
 
 let roomList;
 let selectedList = []
-let roomId = 0;
 let roomSelection = document.getElementById("roomSelection");
-const form = document.getElementById("create-task-form");
 
 document.addEventListener("DOMContentLoaded", () => {
     let previousRoomList, previousRoomListString;
@@ -22,41 +17,33 @@ document.addEventListener("DOMContentLoaded", () => {
         previousRoomList = previousRoomListString.split(',');
     }
     console.log(previousRoomList.type);
-    setRoomList(previousRoomList);
     roomList = previousRoomList
     updateCharCounter("description", "description-length-counter");
     populateRoomSelection();
+    addRoomsToSubmission("create-task-form", selectedList);
     document.getElementById("roomSelection").onclick = () => populateRoomSelection();
+    roomSelection.addEventListener("change", function () { selectRoom(this.value) });
 });
 
 function selectRoom(room) {
     selectedList.push(room);
-    renderRooms(selectedList);
+    renderRooms(selectedList, "room-table");
 }
 
 function populateRoomSelection() {
-    console.log("all: " + roomList);
-    console.log("selected: " + selectedList);
-    let i, length;
-    length = roomSelection.options.length - 1
-    for (i = length; i >= 0; i--) {
-        roomSelection.options.remove(i);
-    }
+    roomSelection.length = 0;
     let unselected = unselectedList();
-    console.log("unselected: " + unselected);
     if (unselected.length > 0) {
-    for (let i = 0; i < roomList.length; i++) {
-        let option = document.createElement("option");
-        option.value = unselected[i];
-        option.textContent = unselected[i];
-        option.onclick = () => selectRoom(unselected[i]);
-        roomSelection.appendChild(option);
+        for (let i = 0; i < roomList.length; i++) {
+            let option = document.createElement("option");
+            option.value = unselected[i];
+            option.textContent = unselected[i];
+            roomSelection.appendChild(option);
+        }
+    } else {
+
     }
-
-}   else {
-
-
-}}
+}
 
 function unselectedList() {
     let i, withoutSelected, selectedRoom;
@@ -67,10 +54,6 @@ function unselectedList() {
         withoutSelected = withoutSelected.filter(thisRoom => thisRoom !== selectedRoom);
     }
     return withoutSelected;
-
-}
-
-function renderSelectedRooms() {
 
 }
 
