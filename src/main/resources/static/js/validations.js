4ca6b0474db2a148c3a12157095ebbcf03257b5c
rafed <rdu46>
2025-03-21 15:10:48 +1300

    refactored for reuse of functions now in validation.js without those remaining in renovation.js

--- src/main/resources/static/js/validations.js
+++ src/main/resources/static/js/validations.js
@@ -1,3 +1,7 @@
+let roomId = 0;
+let roomFieldValid = false;
+let roomList = [];
+
 /**
  * Validates a given input field based off the provided regular expression pattern
  * If the input is valid the error label is hidden, if it isn't the appropriate error message is displayed
@@ -17,4 +21,96 @@ export function validateField(input, pattern, errorLabel, errorMessage) {
         errorLabel.textContent = errorMessage;
         return false;
     }
+}
+
+export function addRoomsToSubmission(formId) {
+    const form = document.getElementById(formId);
+    form.addEventListener("submit", function (event) {
+        let input;
+        event.preventDefault();
+        form.querySelectorAll("input[name='roomList']").forEach(room => room.remove());
+        roomList.forEach(room => {
+            input = document.createElement("input");
+            input.type = "hidden";
+            input.name = "roomList";
+            input.value = room[1];
+            form.appendChild(input);
+        });
+        form.submit();
+    });
+}
+
+export function renderRooms() {
+    let i, room, roomTable, tableRow, roomCell, buttonCell, button;
+    roomTable = document.getElementById("room-list");
+    roomTable.innerHTML = "";
+    for (i=0; i<roomList.length; i++) {
+        room = roomList[i];
+        tableRow = roomTable.insertRow();
+        roomCell = tableRow.insertCell(0);
+        roomCell.innerText = room[1];
+        buttonCell = tableRow.insertCell(1);
+        buttonCell.style.textAlign = "right";
+        buttonCell.style.width = "50px";
+        button = document.createElement("button");
+        button.className = "btn btn-primary";
+        button.innerHTML = "❌";
+        button.style.backgroundColor = "white";
+        button.style.border = "1px solid #ccc";
+        button.style.cursor = "pointer";
+        button.background = "white";
+        button.onclick = function () {
+            roomList = roomList.filter(thisRoom => thisRoom[0] !== room[0]);
+            renderRooms();
+        };
+        buttonCell.appendChild(button);
+    }
+}
+
+export function setRoomList(previousRoomList) {
+    console.log(previousRoomList);
+    if (previousRoomList !== null) {
+        previousRoomList.forEach(room => {
+            roomList.push([roomId++, room]);
+        });
+        renderRooms();
+    }
+}
+
+export function updateCharCounter(textAreaId, counterId, maxLimit = 512) {
+    let textArea = document.getElementById(textAreaId);
+    let counter = document.getElementById(counterId);
+    let length = textArea.value.length;
+    counter.textContent = `${length}/${maxLimit}`;
+}
+
+export function addRoom(inputId) {
+    let input = document.getElementById(inputId);
+    let room = input.value.trim();
+    if (room === "") {
+        // If the room is empty, don't add it to the list, just return.
+        console.log("No room entered.");
+        return;
+    }
+    let roomError = getRoomError(room)
+    if (roomError == null) {
+        input.value = "";
+        roomList.push([roomId++, room]);
+        renderRooms();
+    } else {
+        //error text becomes room error
+    }
+}
+
+export function getRoomError(toAdd) {
+    let error = null;
+    const validCharactersPattern = /^[\p{L}\d .,\-']*$/u;
+    if (!validCharactersPattern.test(toAdd)) {
+        error = "Renovation record room names must only contain letters, numbers, spaces, dots, hyphens or apostrophes";
+    }
+    return error;
+}
+
+export function checkRoomName(input) {
+    roomFieldValid = validateField(input, /^[\p{L}\d .,\-']*$/u, roomErrorLabel, roomNameErrorMessage);
 }
