90ca4370e215d6762dad07d90ebfa3247bf0b01a
Ryan Hamilton <rha206@uclive.ac.nz>
2025-03-24 14:14:49 +1300

    Feature: Connect HTML, controller, service, and validation layers with simple implementation.
    - Check if email is in correct format.
    - Check if email exists.
    - Return appropriate messages based on email input.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/ForgotPasswordController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/ForgotPasswordController.java
@@ -1,16 +1,25 @@
 package nz.ac.canterbury.seng302.homehelper.controller;
 
+import nz.ac.canterbury.seng302.homehelper.service.ForgotPasswordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForgotPasswordController {
 
     Logger logger = LoggerFactory.getLogger(HomeController.class);
+    ForgotPasswordService forgotPasswordService;
+
+    @Autowired
+    public ForgotPasswordController(ForgotPasswordService forgotPasswordService) {
+        this.forgotPasswordService = forgotPasswordService;
+    }
 
     @GetMapping("/password/forgot")
     public String forgotPassword() {
@@ -19,9 +28,15 @@ public class ForgotPasswordController {
     }
 
     @PostMapping("/password/forgot")
-    public String submitEmail(Model model) {
+    public String submitEmail(@RequestParam("email") String email, Model model) {
         logger.info("POST /forgot-password");
-        model.addAttribute("emailMessage", "An email was sent to the address if it was recognised");
+        String errorMessage = forgotPasswordService.validateEmail(email);
+        System.out.println(errorMessage);
+        if (errorMessage.isEmpty()) {
+            model.addAttribute("emailMessage", "An email was sent to the address if it was recognised");
+        } else {
+            model.addAttribute("errorMessage", errorMessage);
+        }
         return "forgotPasswordTemplate";
     }
 }
