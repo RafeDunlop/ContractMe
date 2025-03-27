8ed67a22e7ea0fb1e631d32aa50f3cb464f5e716
jlc185 <jlc185@uclive.ac.nz>
2025-03-27 18:08:28 +1300

    - Added confirm prompt to the html page - Implemented messages displaying in the prompt when searching. - Added first and last button to html - Added documentation to pagination.js

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -1,9 +1,26 @@
-const totalPages = parseInt(document.getElementById('data-total-pages').value, 10);
-function validatePageSearch(recordId) {
+/**
+ * Used in viewRenovation.html
+ * Requires confirmPrompt.js loaded beforehand
+ */
 
+const totalPages = parseInt(document.getElementById('data-total-pages').value, 10);
+
+/**
+ * Onclick method for the task search button on View Renovation
+ * Uses the confirmation prompt before searching
+ * Validates the desired page to visit by checking it's a number and within the bounds of all pages
+ * @param recordId the id of renovation being viewed, used to make the url for a successful search
+ */
+function validatePageSearch(recordId) {
     let desiredPage = parseInt(document.getElementById("pageSearch").value, 10);
+
     if (!isNaN(desiredPage) && desiredPage >= 0 && desiredPage <= totalPages - 1) {
-        window.location.href = "/renovations/view?id=" + recordId + "&page=" + desiredPage;
+        const confirmText= "Are you sure you want to go to page " + desiredPage + "?";
+        confirmPrompt(confirmText, "Confirm", "Cancel", false).then((confirm) => {
+            if (confirm) {
+                window.location.href = "/renovations/view?id=" + recordId + "&page=" + desiredPage;
+            }
+        });
     } else {
         const errorText = "The page number is outside the range of available pages.";
         document.getElementById("errorMessage").style.display = "block";
