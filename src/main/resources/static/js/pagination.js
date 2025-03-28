f10f13c632ff013814a1168b309a1d46a37a0e2a
jlc185 <jlc185@uclive.ac.nz>
2025-03-28 16:29:20 +1300

    - Pair programed with mason mot30 - Changed js to bring up search prompt even on out of bounds

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -14,16 +14,16 @@ const totalPages = parseInt(document.getElementById('data-total-pages').value, 1
 function validatePageSearch(recordId) {
     let desiredPage = parseInt(document.getElementById("pageSearch").value, 10);
 
-    if (!isNaN(desiredPage) && desiredPage >= 1 && desiredPage <= totalPages) {
-        const confirmText= "Are you sure you want to go to page " + desiredPage + "?";
-        confirmPrompt(confirmText, "Confirm", "Cancel", false).then((confirm) => {
-            if (confirm) {
+    const confirmText= "Are you sure you want to go to page " + desiredPage + "?";
+    confirmPrompt(confirmText, "Confirm", "Cancel", false).then((confirm) => {
+        if (confirm) {
+            if (!isNaN(desiredPage) && desiredPage >= 1 && desiredPage <= totalPages) {
                 window.location.href = "/renovations/view?id=" + recordId + "&page=" + desiredPage;
+            } else {
+                const errorText = "The page number is outside the range of available pages.";
+                document.getElementById("errorMessage").style.display = "block";
+                document.getElementById("errorText").innerText = errorText;
             }
-        });
-    } else {
-        const errorText = "The page number is outside the range of available pages.";
-        document.getElementById("errorMessage").style.display = "block";
-        document.getElementById("errorText").innerText = errorText;
-    }
+        }
+    });
 }
