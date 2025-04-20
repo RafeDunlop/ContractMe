f3830f6002558c51f43a7dd5621e9b10070cc84f
rafed <rdu46@uclive.ac.nz>
2025-04-20 15:34:23 +1200

    -added a method to get the fragment id

--- src/main/resources/static/js/addTaskIcon.js
+++ src/main/resources/static/js/addTaskIcon.js
@@ -6,18 +6,23 @@ function addTaskIcon(button) {
     button.classList.add("active");
 }
 
-function showIconSelector() {
-    const overlay = document.getElementById("icon-selector");
+function getIconSelector(renovationTaskId) {
+    const modalId = "icon-selector-" + renovationTaskId;
+    return document.getElementById(modalId);
+}
+
+function showIconSelector(renovationTaskId) {
+    const overlay = getIconSelector(renovationTaskId)
     overlay.style.display = 'block';
 }
 
-async function submitIcon() {
-    const overlay = document.getElementById("icon-selector");
+async function submitIcon(taskId) {
+    const overlay = getIconSelector(taskId);
     overlay.style.display = 'none';
     const button = document.querySelector(".icon-btn.active");
     const iconFileName = button.id;
     const csrfToken = button.getAttribute("data-csrf");
-    const taskId = button.getAttribute("data-taskid");
+    taskId = button.getAttribute("data-taskid");
     const response = await fetch(`/editTask/edit-icon/${taskId}`, {
         method: "POST",
         headers: {'X-CSRF-TOKEN': csrfToken, 'Content-Type': 'application/json'},
@@ -29,10 +34,10 @@ async function submitIcon() {
 }
 
 async function deleteIcon(button) {
-    const overlay = document.getElementById("icon-selector");
-    overlay.style.display = 'none';
     const csrfToken = button.getAttribute("data-csrf");
     const taskId = button.getAttribute("data-taskid");
+    const overlay = getIconSelector(taskId);
+    overlay.style.display = 'none';
     const response = await fetch(`/editTask/edit-icon/${taskId}`, {
         method: "POST",
         headers: {'X-CSRF-TOKEN': csrfToken, 'Content-Type': 'application/json'},
