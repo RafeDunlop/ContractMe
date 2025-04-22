b6d4252bfdcc31e26833d782b997f36b9f346d7c
Jack Guard <jgu73@uclive.ac.nz>
2025-04-22 21:31:59 +1200

    Add rudimentary input error highlighting, still incomplete. Burnt a lot of time investigating bugs.

--- src/main/resources/static/js/updatePasswordValidation.js
+++ src/main/resources/static/js/updatePasswordValidation.js
@@ -14,22 +14,35 @@ let newPasswordRetypedFrontendError = document.getElementById("new-password-rety
 let newPasswordRetypedFrontendErrorMessage = document.getElementById("new-password-retyped-frontend-error-message")
 let newPasswordRetypedBackendError = document.getElementById("new-password-retyped-backend-error")
 
+
 //Event listeners
 oldPasswordField.addEventListener("input", () => checkOldPasswordField());
 newPasswordField.addEventListener("input", () => checkPasswordStrength());
 newPasswordRetypedField.addEventListener("input", () => checkPasswordMatch());
 
+/*oldPasswordField.addEventListener("input", () => clearInputError());
+newPasswordField.addEventListener("input", () => clearInputError());
+newPasswordRetypedField.addEventListener("input", () => clearInputError());
+
+
+
+function clearInputError() {
+    //Toggle off the input error class on the field that calls it, will implement
+    //when the toggle on has been debugged
+}*/
 
 function checkOldPasswordField() {
 
 }
 
 function checkPasswordStrength() {
+    console.log("Check password strength");
     const password = newPasswordField.value;
 
     const strengthError = "Your password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.";
 
     let passwordErrors = [];
+
 
     if (
         password.length < 8 ||
@@ -42,10 +55,12 @@ function checkPasswordStrength() {
     }
 
     if (passwordErrors.length > 0) {
+        console.log("New password error");
         newPasswordFrontendErrorMessage.textContent = passwordErrors[0];
         newPasswordFrontendError.hidden = false;
         newPasswordFrontendErrorMessage.hidden = false;
         newPasswordBackendError.hidden = true;
+        newPasswordField.style.borderColor = "red";
     } else {
         newPasswordFrontendErrorMessage.textContent = "";
         newPasswordFrontendError.hidden = true;
@@ -55,14 +70,17 @@ function checkPasswordStrength() {
 }
 
 function checkPasswordMatch() {
+    console.log("Check password match")
     const password = newPasswordField.value;
     const confirmPassword = newPasswordRetypedField.value;
 
     if (password !== confirmPassword) {
+        console.log("retyped password error");
         newPasswordRetypedFrontendErrorMessage.textContent = "Passwords do not match.";
         newPasswordRetypedFrontendError.hidden = false;
         newPasswordRetypedFrontendErrorMessage.hidden = false;
         newPasswordRetypedBackendError.hidden = true;
+        newPasswordField.style.borderColor = "red";
     } else {
         newPasswordRetypedFrontendErrorMessage.textContent = "";
         newPasswordRetypedFrontendError.hidden = true;
