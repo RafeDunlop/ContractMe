eab443ad14cc318e003db7adde914a2a6ac207b3
Jack Guard <jgu73@uclive.ac.nz>
2025-05-02 14:50:58 +1200

    Revert "The majority of the sample code for this spike solution has been implemented, including an environment variable accessor."
    This reverts commit 5249efb131b9d47631c1bdb5617d30f8e19b636e.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/validation/ProfanitySpikeValidation.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/validation/ProfanitySpikeValidation.java
@@ -1,40 +1,16 @@
 package nz.ac.canterbury.seng302.homehelper.validation;
 
-import java.io.*;
-
-import nz.ac.canterbury.seng302.homehelper.util.EnvVarUtil;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfanitySpikeValidation {
 
-    private final EnvVarUtil envVarUtil = new EnvVarUtil();
 
-    public void validateTagName(String tagName) throws IOException {
+    public void validateTagName(String tagName) {
 
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, tagName);
-
-        Request request = new Request.Builder()
-                .url("https://api.apilayer.com/bad_words?censor_character=censor_character")
-                .addHeader("apikey", envVarUtil.retrieveEnvironmentVariable("BAD_FILTER_API"))
-                .method("POST", body)
-                .build();
-        Response response = client.newCall(request).execute();
-    System.out.println(response.body().string());
 
 
     }
-
-
 
 
 }
