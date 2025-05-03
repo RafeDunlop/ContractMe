2023300da87a104fce9b1c2da4c1c2f0342cdc7a
rafed <rdu46@uclive.ac.nz>
2025-05-03 20:19:15 +1200

    Cleanup removed alternative class

--- src/main/java/nz/ac/canterbury/seng302/homehelper/service/LocationService.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/service/LocationService.java
@@ -2,6 +2,7 @@ package nz.ac.canterbury.seng302.homehelper.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -9,30 +10,31 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Basic unlinked location implementation using iPapi and Google Places
+ * Basic unlinked location implementation using Geoapify
  * @author Rafe Dunlop
  */
-public class LocationServiceIpapiPlaces {
+@Service
+public class LocationService {
 
-    private static final String ipapiBaseUrl = "https://ipapi.co/";
+    private static final String geoapifyBaseUrl = "https://api.geoapify.com/v1/";
 
-    private static final String placesJsonAutocompleteUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+    private static final String ipApi = "ipinfo";
 
-    private static final int locationBiasRadiusMetres = 500000; //500 km
+    private static final String autocompleteApi = "geocode/autocomplete";
 
-    private static final long placesApiKey = -1; //redacted
+    private static final long apiKey = -1; // redacted
 
-    private static final Logger logger = LoggerFactory.getLogger(LocationServiceIpapiPlaces.class);
+    private static final Logger logger = LoggerFactory.getLogger(LocationService.class);
 
     /**
-     * Gets localisation information using an iPapi API
-     * @param ip The IP address to retrieve the localisation of
-     * @throws IOException If the request is not successful
+     * Retrieves the localisation of a client from their ip address
+     * @param ip The ip address of the client machine
+     * @throws IOException if the request is unsuccessful
      */
     public void getRoughLocation(Long ip) throws IOException {
         URL url = getUrlIpGrab(ip);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("User-Agent", "java-ipapi-v1.02");
+        connection.setRequestProperty("Accept", "application/json");
         logger.info(connection.getResponseMessage());
     }
 
@@ -45,20 +47,24 @@ public class LocationServiceIpapiPlaces {
      * @throws IOException if the request is unsuccessful
      */
     public void getAutocomplete(String prompt, String countryCode, double latitude, double longitude) throws IOException {
-        URL url = getAutocompleteUrl(prompt, countryCode, latitude, longitude);;
+        URL url = getAutoCompleteUrl(prompt, countryCode, latitude, longitude);;
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestProperty("Accept", "application/json");
         logger.info(connection.getResponseMessage());
     }
 
-    private URL getAutocompleteUrl(String prompt, String countryCode, double latitude, double longitude) {
+
+    private URL getAutoCompleteUrl(String prompt, String countryCode, double latitude, double longitude) {
         StringBuilder sb = new StringBuilder();
-        sb.append(placesJsonAutocompleteUrl);
-        sb.append(String.format("?input=%s", prompt));
-        sb.append(String.format("&location=%f-%f", latitude, longitude));
-        sb.append(String.format("&locationbias=%d@%f,%f", locationBiasRadiusMetres, latitude, longitude));
-        sb.append(String.format("&components=country:%s", countryCode));
-        sb.append(String.format("&key=%d", placesApiKey));
+        sb.append(geoapifyBaseUrl);
+        sb.append(autocompleteApi);
+        sb.append(String.format("?text=%s", prompt));
+        sb.append(String.format("&filter=countrycode:%s", countryCode));
+        sb.append(String.format("&bias=proximity:%f,%f", latitude, longitude));
+        sb.append(String.format("&apiKey=%d", apiKey));
+        sb.append("&type=street");
+        sb.append("&lang=en");
+        sb.append("&format=json");
         try {
             return new URL(sb.toString());
         } catch (MalformedURLException e) {
@@ -69,8 +75,9 @@ public class LocationServiceIpapiPlaces {
 
     private URL getUrlIpGrab(Long ip) {
         StringBuilder sb = new StringBuilder();
-        sb.append(ipapiBaseUrl);
-        sb.append(String.format("/%d/json/", ip));
+        sb.append(geoapifyBaseUrl);
+        sb.append(ipApi);
+        sb.append(String.format("?ip=%d&apiKey=%d", ip, apiKey));
         try {
             return new URL(sb.toString());
         } catch (MalformedURLException e) {
@@ -78,4 +85,5 @@ public class LocationServiceIpapiPlaces {
             throw new IllegalArgumentException("Malformed URL");
         }
     }
+
 }
