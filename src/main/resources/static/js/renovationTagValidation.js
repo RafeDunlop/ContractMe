a85edd083dd3af9a1f2e2c52d449c2fb549c8087
Ryan Hamilton <rha206@uclive.ac.nz>
2025-05-06 17:29:04 +1200

    Feature: Added frontend validation the view renovation page, for tags.

--- src/main/resources/static/js/renovationTagValidation.js
+++ src/main/resources/static/js/renovationTagValidation.js
@@ -1,8 +1,37 @@
-let tagNameField = document.getElementById("description-frontend-error-message")
 
-tagNameField.addEventListener("input", function () {validateTag(recordNameField.value)})
+let tagInput = document.getElementById("tagName");
+let tagFrontendError = document.getElementById("tag-frontend-error");
+let tagFrontendErrorMessage = document.getElementById("tag-frontend-error-message");
+let tagBackendError = document.getElementById("tag-backend-error");
 
+const tagNameListElements = document.querySelectorAll('#tag-list span');
+const tagNamesList = Array.from(tagNameListElements).map(el => el.textContent.trim());
+
+
+const tagPattern = /^(?=.*\p{L}).*$/u;
+
+tagInput.addEventListener("input", function () {validateTag(tagInput.value)})
 
 function validateTag(input) {
+    console.log(tagNamesList);
+
+    input = input.trim();
+    if (input.length > 128) {
+        tagFrontendErrorMessage.textContent = "Tag cannot be greater than 128 characters.";
+        tagFrontendError.hidden = false;
+        tagBackendError.hidden = true;
+        return false;
+    } else if (input === "" || !tagPattern.test(input)) {
+        tagFrontendErrorMessage.textContent = "Tags must contain one or more letters.";
+        tagFrontendError.hidden = false;
+        tagBackendError.hidden = true;
+        return false;
+    } else {
+        tagFrontendErrorMessage.textContent = "";
+        tagFrontendError.hidden = true;
+        tagBackendError.hidden = true;
+        return true;
+    }
+
 
 }
