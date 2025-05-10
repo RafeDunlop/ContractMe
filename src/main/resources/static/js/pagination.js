595890d237f22566d5e7756a02089f2ef8e52035
Sean Reitsma <sre114@uclive.ac.nz>
2025-05-10 18:35:35 +1200

    feat: implement pagination on My Renovations
    Validate search has been put in its own file. Javascript specific to the
    my renovation page is in `renovationPagination.js`. The id query param
    in the url for the pagination footer is being set to null on the my
    renovation page because I couldn't work out a better way to be able to
    usue the same fragment for both the task cards and the my renovations
    page. I think that won't matter in the end because the task card
    pagination will change anyway.

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -3,37 +3,6 @@
  * Requires confirmPrompt.js loaded beforehand
  */
 
-
-
-/**
- * Onclick function for the task search button on View Renovation
- * Uses the confirmation prompt before searching
- * Validates the desired page to visit by checking it's a number and within the bounds of all pages
- * @param recordId the id of renovation being viewed, used to make the url for a successful search
- */
-
-function validatePageSearch(recordId) {
-    let desiredPage = document.getElementById("pageSearch").value;
-    desiredPage = parseInt(desiredPage, 10);
-
-    const totalPages = parseInt(document.getElementById('data-total-pages').value, 10);
-    const confirmText= "Are you sure you want to go to page " + desiredPage.toString() + "?";
-    confirmPrompt(confirmText, "Confirm", "Cancel", false).then((confirm) => {
-        if (confirm) {
-            if (!isNaN(desiredPage) && desiredPage >= 1 && desiredPage <= totalPages) {
-                let url = new URL(window.location.href);
-                url.searchParams.set("page", desiredPage);
-                url.searchParams.set("id", recordId);
-                window.location.href = url.toString();
-            } else {
-                const errorText = "The page number is outside the range of available pages.";
-                document.getElementById("errorMessage").style.display = "block";
-                document.getElementById("errorText").innerText = errorText;
-            }
-        }
-    });
-}
-
 /**
  * This function updates the layout based on the window size and adjusts the number of tasks to be displayed on the page.
  */
