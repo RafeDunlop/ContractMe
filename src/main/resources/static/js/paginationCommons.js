f9e0163168c584bcc59389ccb807cf54b2d216f1
Sean Reitsma <sre114@uclive.ac.nz>
2025-05-12 18:05:44 +1200

    fix: UX fixes for pagination
    - Change "Search" button to say "Go" (as recommended by Miguel).
    - Change the input validation to not show the confirm modal if a
      non-number value is entered.

--- src/main/resources/static/js/paginationCommons.js
+++ src/main/resources/static/js/paginationCommons.js
@@ -10,18 +10,24 @@ function validatePageSearch(recordId) {
 
     const totalPages = parseInt(document.getElementById('data-total-pages').value, 10);
     const confirmText= "Are you sure you want to go to page " + desiredPage.toString() + "?";
-    confirmPrompt(confirmText, "Confirm", "Cancel", false).then((confirm) => {
-        if (confirm) {
-            if (!isNaN(desiredPage) && desiredPage >= 1 && desiredPage <= totalPages) {
-                let url = new URL(window.location.href);
-                url.searchParams.set("page", desiredPage);
-                url.searchParams.set("id", recordId);
-                window.location.href = url.toString();
-            } else {
-                const errorText = "The page number is outside the range of available pages.";
-                document.getElementById("errorMessage").style.display = "block";
-                document.getElementById("errorText").innerText = errorText;
+    if (!isNaN(desiredPage)) {
+        confirmPrompt(confirmText, "Confirm", "Cancel", false).then((confirm) => {
+            if (confirm) {
+                if (desiredPage >= 1 && desiredPage <= totalPages) {
+                    let url = new URL(window.location.href);
+                    url.searchParams.set("page", desiredPage);
+                    url.searchParams.set("id", recordId);
+                    window.location.href = url.toString();
+                } else {
+                    const errorText = "The page number is outside the range of available pages.";
+                    document.getElementById("errorMessage").style.display = "block";
+                    document.getElementById("errorText").innerText = errorText;
+                }
             }
-        }
-    });
+        });
+    } else {
+        const errorText = "The page number must be a number.";
+        document.getElementById("errorMessage").style.display = "block";
+        document.getElementById("errorText").innerText = errorText;
+    }
 }
