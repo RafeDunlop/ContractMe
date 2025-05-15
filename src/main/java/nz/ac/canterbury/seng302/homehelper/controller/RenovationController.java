40c1cdafd0d231e17e1845506669ea96c3a289e1
Sean Reitsma <sre114@uclive.ac.nz>
2025-05-15 13:54:33 +1200

    fix(My Renovation): fix url when searchQuery is empty
    Fixed the pagination redirect for the My Renovations
    page to not add the search query parameter if it is empty.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/RenovationController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/RenovationController.java
@@ -73,7 +73,7 @@ public class RenovationController {
                 itemsPerPage = 8;
             }
             if (pageNumber < 1) {
-                return "redirect:/renovations?page=1&itemsPerPage=" + itemsPerPage + "&searchQuery=" + searchQuery;
+                return "redirect:/renovations?page=1&itemsPerPage=" + itemsPerPage + (!searchQuery.isEmpty() ? "&searchQuery=" + searchQuery : "");
             }
 
             User user = loginService.getUserByEmail();
@@ -85,7 +85,7 @@ public class RenovationController {
             int paginationLinksEnd = Math.min(pageNumber + 2, totalPages);
 
             if (pageNumber > totalPages && totalRenovations != 0) {
-                return "redirect:/renovations?page=" + totalPages + "&itemsPerPage=" + itemsPerPage + "&searchQuery=" + searchQuery;
+                return "redirect:/renovations?page=" + totalPages + "&itemsPerPage=" + itemsPerPage + (!searchQuery.isEmpty() ? "&searchQuery=" + searchQuery : "");
             }
             model.addAttribute("renovations", renovationRecords.getContent());
             model.addAttribute("paginationLinksStart", paginationLinksStart);
