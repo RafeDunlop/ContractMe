1a611e91216ca028aaa4b7c33e28e01717485ff9
Jake Connolly <jlc185@uclive.ac.nz>
2025-05-15 12:42:57 +1200

    test: Finished integration tests for tag searching. Added test for no matching public records. And added test to check results are displayed in correct order sorted by number of matching tags.,

--- src/test/java/nz/ac/canterbury/seng302/homehelper/integration/controller/RenovationControllerIntegrationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/homehelper/integration/controller/RenovationControllerIntegrationTest.java
@@ -1120,15 +1120,17 @@ public class RenovationControllerIntegrationTest {
     }
 
     @Test
-    public void tagSearch_withValidList_displaysListOfTags() throws Exception {
+    public void tagSearch_withValidPublicRenovation_displaysListOfTags() throws Exception {
         RenovationRecord testRecord = new RenovationRecord(owner, "Test Renovation", "Room A Renovation", List.of("Room A"));
         testRecord.setPublicity(true);
+        RenovationRecord testRecord2 = new RenovationRecord(owner, "Test Renovation", "Room A Renovation", List.of("Room A"));
         String tagName ="House";
         Tag testTag = new Tag(tagName);
         tagRepository.save(testTag);
-
         testRecord.getTags().add(testTag);
+        testRecord2.getTags().add(testTag);
         renovationRecordRepository.save(testRecord);
+        renovationRecordRepository.save(testRecord2);
 
         MvcResult postResult = mockMvc.perform(post("/renovations/search")
                         .param("tagNameList", tagName)
@@ -1146,6 +1148,70 @@ public class RenovationControllerIntegrationTest {
                 .andExpect(model().attribute("records", hasSize(1)))
                 .andExpect(model().attribute("records", hasItem(
                         hasProperty("name", is("Test Renovation")))));
+    }
 
+    @Test
+    public void tagSearch_withNoPublicRenovation_NoResults() throws Exception {
+        RenovationRecord testRecord = new RenovationRecord(owner, "Test Renovation", "Room A Renovation", List.of("Room A"));
+        String tagName ="Apartment";
+        Tag testTag = new Tag(tagName);
+        tagRepository.save(testTag);
+        testRecord.getTags().add(testTag);
+        renovationRecordRepository.save(testRecord);
+
+        MvcResult postResult = mockMvc.perform(post("/renovations/search")
+                        .param("tagNameList", tagName)
+                        .param("isTagSearch", "true")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/renovations/search?page=1"))
+                .andReturn();
+
+        mockMvc.perform(get("/renovations/search")
+                        .session((MockHttpSession) Objects.requireNonNull(postResult.getRequest().getSession(false))))
+                .andExpect(status().isOk())
+                .andExpect(view().name("renovationSearchTemplate"))
+                .andExpect(model().attributeExists("records"))
+                .andExpect(model().attribute("records", hasSize(0)));
+    }
+
+    @Test
+    public void tagSearch_withValidPublicRenovation_displaysMoreMatchingTagsFirstListOfTags() throws Exception {
+        RenovationRecord testRecord = new RenovationRecord(owner, "RenovationOneTag", "Room A Renovation", List.of("Room A"));
+        RenovationRecord testRecord2 = new RenovationRecord(owner, "RenovationTwoTags", "Room A Renovation", List.of("Room A"));
+        testRecord.setPublicity(true);
+        testRecord2.setPublicity(true);
+
+        String tagName ="House";
+        String tagName2 ="New";
+        Tag testTag = new Tag(tagName);
+        Tag testTag2 = new Tag(tagName2);
+        tagRepository.save(testTag);
+        tagRepository.save(testTag2);
+
+        testRecord.getTags().add(testTag);
+        testRecord2.getTags().add(testTag);
+        testRecord2.getTags().add(testTag2);
+        renovationRecordRepository.save(testRecord);
+        renovationRecordRepository.save(testRecord2);
+
+        MvcResult postResult = mockMvc.perform(post("/renovations/search")
+                        .param("tagNameList", tagName)
+                        .param("tagNameList", tagName2)
+                        .param("isTagSearch", "true")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/renovations/search?page=1"))
+                .andReturn();
+
+        mockMvc.perform(get("/renovations/search")
+                        .session((MockHttpSession) Objects.requireNonNull(postResult.getRequest().getSession(false))))
+                .andExpect(status().isOk())
+                .andExpect(view().name("renovationSearchTemplate"))
+                .andExpect(model().attribute("records", hasSize(2)))
+                .andExpect(model().attribute("records", contains(
+                        hasProperty("name", is("RenovationTwoTags")),
+                        hasProperty("name", is("RenovationOneTag"))
+                )));
     }
 }
