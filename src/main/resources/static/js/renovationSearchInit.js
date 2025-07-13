50449f60fe488bf0e43ef9c2b95ab380bfdbd35f
Mason Ott <mot30@uclive.ac.nz>
2025-07-13 20:02:56 +1200

    Move listener logic to new javaScript file, also added missing renovationsTemplate listener logic

---
+++ src/main/resources/static/js/renovationSearchInit.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setupTagSearch();
+    updateLayout();
+    window.addEventListener('resize', () => {
+        clearTimeout(window._resizeTimeout);
+        cachedCardSize = null;
+        window._resizeTimeout = setTimeout(updateLayout, 400);
+    });
+
+    lastSubmittedSearchTerm = document.querySelector("input[name='searchTerm']").value.trim();
+    lastSubmittedTags = Array.from(document.querySelectorAll("#hidden-tag-inputs input[name='tagNameList']")).map(input => input.value);
+
+    document.getElementById("search-form").addEventListener("submit", function (e) {
+        e.preventDefault();
+        fetchRenovations("cards", true);
+    });
+});
