0a2e5cef88f966d4bf65af13e579689cffb38590
abhi <ach331@uclive.ac.nz>
2025-07-16 21:36:05 +1200

    Fix: - Error showing on browser console fixed.

--- src/main/resources/static/js/tagSearch.js
+++ src/main/resources/static/js/tagSearch.js
@@ -24,7 +24,7 @@ tagInput.addEventListener("input", function () {
         resetAutocomplete();
         return
     }
-    updateAutocomplete(partialTag);
+    updateSearchAutocomplete(partialTag)
 });
 
 /**
