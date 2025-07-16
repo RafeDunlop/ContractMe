ed0a9b1868336265d968c6317b0dcdc564c66444
Mason Ott <mot30@uclive.ac.nz>
2025-07-17 00:10:31 +1200

    Remove duplicated if statements

--- src/main/resources/static/js/taskValidation.js
+++ src/main/resources/static/js/taskValidation.js
@@ -47,48 +47,43 @@ form.addEventListener("submit", function (e) {
 
 function validateName(input) {
     input = input.trim();
+    if (nameBackendError) nameBackendError.hidden = true;
     if (input.length > 128) {
         setError(nameFrontendError, nameFrontendErrorMessage, "Task name cannot be greater than 128 characters");
-        if (nameBackendError) nameBackendError.hidden = true;
         return false;
     } else if (input === "" || !namePattern.test(input)) {
         setError(nameFrontendError, nameFrontendErrorMessage, "Task name cannot be empty and must only include letters, numbers, spaces, dots, hyphens or apostrophes");
-        if (nameBackendError) nameBackendError.hidden = true;
         return false;
     } else {
         clearError(nameFrontendError, nameFrontendErrorMessage);
-        if (nameBackendError) nameBackendError.hidden = true;
         return true;
     }
 }
 
 function validateDescription(input) {
     input = input.trim();
+    if (descriptionBackendError) descriptionBackendError.hidden = true;
     if (input === "") {
         setError(descriptionFrontendError, descriptionFrontendErrorMessage, "Task description cannot be empty.");
-        if (descriptionBackendError) descriptionBackendError.hidden = true;
         return false;
     } else if (input.length > maxDescriptionLength) {
         setError(descriptionFrontendError, descriptionFrontendErrorMessage, `Task description must be ${maxDescriptionLength} characters or less.`);
-        if (descriptionBackendError) descriptionBackendError.hidden = true;
         return false;
     } else {
         clearError(descriptionFrontendError, descriptionFrontendErrorMessage);
-        if (descriptionBackendError) descriptionBackendError.hidden = true;
         return true;
     }
 }
 
 function validateDueDate(dateValue) {
+    if (dueDateBackendError) dueDateBackendError.hidden = true;
     if (dateValue && new Date(dateValue) < new Date()) {
         dueDateFrontendError.hidden = false;
         dueDateFrontendErrorMessage.innerText = "Due date must be in the future.";
-        if (dueDateBackendError) dueDateBackendError.hidden = true; // Hide backend error when frontend validation fails
         return false;
     }
 
     dueDateFrontendError.hidden = true;
-    if (dueDateBackendError) dueDateBackendError.hidden = true; // Show backend error if set (this is only needed when backend validation has issues)
     return true;
 }
 
