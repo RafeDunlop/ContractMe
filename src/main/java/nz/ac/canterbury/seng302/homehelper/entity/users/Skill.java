4765724d77e7d08229ca6943aea6b88ffea0e74d
Ryan <rha206@uclive.ac.nz>
2025-07-19 16:02:04 +1200

    Feature: Add method to get enum from display name in skill enum. Change hover colour of delete button for bubbles.
    Signed-off-by: Ryan <rha206@uclive.ac.nz>

--- src/main/java/nz/ac/canterbury/seng302/homehelper/entity/users/Skill.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/entity/users/Skill.java
@@ -1,7 +1,7 @@
 package nz.ac.canterbury.seng302.homehelper.entity.users;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -94,6 +94,10 @@ public enum Skill {
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public Skill getEnum(String displayName) {
+        return Stream.of(Skill.values()).filter(skill -> Objects.equals(skill.getDisplayName(), displayName)).findFirst().orElse(null);
     }
 
     public static List<String> listOfSkills() {
