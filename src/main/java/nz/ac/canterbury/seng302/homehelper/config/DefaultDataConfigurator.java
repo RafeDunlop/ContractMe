39371416064d7486166ba6d194bf161602b41796
Jack Guard <jgu73@uclive.ac.nz>
2025-07-21 19:49:45 +1200

    fix: calendar resizing bug
    Manipulated css styling to make text wrap inside the task bubble
    
    Also tweaked the default data slightly to move the most important test renovation back to the front, and to make longer task descriptions

--- src/main/java/nz/ac/canterbury/seng302/homehelper/config/DefaultDataConfigurator.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/config/DefaultDataConfigurator.java
@@ -86,13 +86,7 @@ public class DefaultDataConfigurator {
     }
 
     private void setupDefaultRenovations() {
-        default1Renovation1 = renovationRecordService.addRenovationRecord(
-                new RenovationRecord(default1,
-                        "Jack Erskine revamp",
-                    "CSSE building => palace of slay",
-                        defaultJERooms
-                )
-        );
+
 
         default2Renovation1 = renovationRecordService.addRenovationRecord(
                 new RenovationRecord(default2,
@@ -123,7 +117,17 @@ public class DefaultDataConfigurator {
             record.setPublicity(true);
             renovationRecordService.addRenovationRecord(record);
         }
+
+        default1Renovation1 = renovationRecordService.addRenovationRecord(
+                new RenovationRecord(default1,
+                        "Jack Erskine revamp",
+                        "CSSE building => palace of slay",
+                        defaultJERooms
+                )
+        );
     }
+
+
 
     private void setupDefaultRenovationTasks() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -136,12 +140,19 @@ public class DefaultDataConfigurator {
         );
         renovationTaskService.addRenovationTask(renovationTask, default2Renovation1);
 
+
+        //Counter is to make multiple tasks on the same day; also so that every so often a
+        //task generates with a longer name to test text wrapping of task bubble
         int counter = 1;
         for (int i = 1; i < numGenericTasksToAdd + 1; i++) {
-            if (counter > 8) {
+            if (counter > 18) {
                 counter = 3;
+                renovationTask.setName("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
             }
-            renovationTask.setName(String.format("Renovation Task %d", i));
+            else {
+                renovationTask.setName(String.format("Renovation Task %d: ", i));
+            }
             renovationTask.setDescription(String.format("Renovation Task Description %d", i));
             renovationTask.setDueDate(LocalDate.now().plusDays(counter).format(formatter));
             renovationTask.setRooms(defaultJERooms);
