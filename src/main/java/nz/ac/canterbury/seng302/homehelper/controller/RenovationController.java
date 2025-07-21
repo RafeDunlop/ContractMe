bb33890cb2d094a901d1e29a4a23f8f985938c54
Mason Ott <mot30@uclive.ac.nz>
2025-07-21 16:02:48 +1200

    Fix !14 'back to search' button to always take user back to correct search page

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/RenovationController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/RenovationController.java
@@ -648,7 +648,7 @@ public class RenovationController {
                                                     HttpServletRequest request) {
 
         // Apply default values
-        if (visibility == null) visibility = "user";
+        if (visibility == null) visibility = "all";
         if (searchTerm == null) searchTerm = "";
 
         // Convert tag names to Tag entities if provided
