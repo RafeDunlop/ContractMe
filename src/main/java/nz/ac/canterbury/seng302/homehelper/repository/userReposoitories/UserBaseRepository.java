821d61d19675a7e27ebd7a72fcefd1e4da2d8a98
ach331 <ach331@uclive.ac.nz>
2025-07-31 19:51:34 +1200

    Feature: Contractor Availability checkbox - Added checkbox to change availability of contractor - Checkbox creates a post request to contractorAvailability.js which in return makes a request a endpoint in the ProfileController. - This changes finds the contractor in the repository and changes availability.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/repository/userReposoitories/UserBaseRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/repository/userReposoitories/UserBaseRepository.java
@@ -1,5 +1,6 @@
 package nz.ac.canterbury.seng302.homehelper.repository.userReposoitories;
 
+import nz.ac.canterbury.seng302.homehelper.entity.RenovationRecord;
 import nz.ac.canterbury.seng302.homehelper.entity.users.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
@@ -19,5 +20,12 @@ import java.util.Optional;
 public interface UserBaseRepository<T extends User> extends CrudRepository<T, Long> {
 
     Optional<T> findByEmailIgnoreCase(String email);
+
+    /**
+     *  Finds a contractor from the repository by id
+     * @param id of the contractor to find
+     * @return an Optional containing the contractor if it exists
+     */
+    Optional<RenovationRecord> findById(long id);
 
 }
