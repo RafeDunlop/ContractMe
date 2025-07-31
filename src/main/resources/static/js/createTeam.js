52e401594aa14c28f34d386ae7aa7244e954d7c5
rdu46 <rdu46@uclive.ac.nz>
2025-07-31 17:44:56 +1200

    added missing docstring

--- src/main/resources/static/js/createTeam.js
+++ src/main/resources/static/js/createTeam.js
@@ -1,5 +1,6 @@
 /**
- * todo
+ * Adds a skill field with a hidden input, display and delete button to a specified empty div
+ * input value is enum value, e.g. MANUAL_LABOURER, named skill
  */
 function addSkill() {
     const skillDropdown = document.getElementById("skills-select");
