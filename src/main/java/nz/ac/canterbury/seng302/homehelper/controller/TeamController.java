9d10ffe31d8ab44337efc634532f93d18685b61d
rdu46 <rdu46@uclive.ac.nz>
2025-07-31 14:55:40 +1200

    added check for location presence

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/TeamController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/TeamController.java
@@ -5,6 +5,7 @@ import nz.ac.canterbury.seng302.homehelper.dto.CreateTeamDTO;
 import nz.ac.canterbury.seng302.homehelper.entity.RenovationRecord;
 import nz.ac.canterbury.seng302.homehelper.entity.users.Skill;
 import nz.ac.canterbury.seng302.homehelper.entity.users.User;
+import nz.ac.canterbury.seng302.homehelper.service.LocationService;
 import nz.ac.canterbury.seng302.homehelper.service.LoginService;
 import nz.ac.canterbury.seng302.homehelper.service.RenovationRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,18 +24,20 @@ public class TeamController {
 
     private final RenovationRecordService renovationRecordService;
     private final LoginService loginService;
+    private final LocationService locationService;
 
     @Autowired
-    public TeamController(RenovationRecordService renovationRecordService, LoginService loginService) {
+    public TeamController(RenovationRecordService renovationRecordService, LoginService loginService, LocationService locationService) {
         this.renovationRecordService = renovationRecordService;
         this.loginService = loginService;
+        this.locationService = locationService;
     }
 
     @GetMapping("/create")
     public String createTeam(@RequestParam Long id, @ModelAttribute CreateTeamDTO createTeamDTO, Model model) {
         User loggedIn = loginService.getUserByEmail();
         RenovationRecord renovationRecord = renovationRecordService.getRecordById(id);
-        if (loggedIn != renovationRecord.getUser()) {
+        if (loggedIn != renovationRecord.getUser() | ! locationService.hasLocation(renovationRecord)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         //todo check that record doesn't already have a a team!
