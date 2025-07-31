2ccfd3f11dc5792326e96526a6e3b8d88210d424
rdu46 <rdu46@uclive.ac.nz>
2025-07-31 14:55:25 +1200

    fixed styling and added access to option element rather than selection text, allows differentiation between enum and its displayText

--- src/main/resources/static/js/createTeam.js
+++ src/main/resources/static/js/createTeam.js
@@ -1,24 +1,38 @@
+/**
+ * todo
+ */
 function addSkill() {
     const skillDropdown = document.getElementById("skills-select");
     const selectedSkill = skillDropdown.value;
-    const selectedSkillName = skillDropdown.getAttribute("display-name");
+    const selectedSkillName = skillDropdown.options[skillDropdown.selectedIndex].dataset.displayname;
+    const selectedSkillsDiv = document.getElementById("selected-skills");
+
     const selectedSkillDisplay = document.createElement("div");
-    const selectedSkillInput = document.createElement("input");
-    selectedSkillDisplay.appendChild(selectedSkillInput);
-    selectedSkillInput.value = selectedSkill;
-    selectedSkillInput.textContent = selectedSkillName;
-    selectedSkillInput.readOnly = true;
-    selectedSkillInput.className = "form-control";
-    selectedSkillInput.name = "skill";
-    selectedSkillDisplay.className = "input-group my-4";
+    const selectedSkillFeedback = document.createElement("div");
+    const selectedSkillText = document.createElement("p");
+    const selectedSkillInputHidden = document.createElement("input");
     const deleteButton = document.createElement("button");
+
+    selectedSkillInputHidden.value = selectedSkill;
+    selectedSkillInputHidden.type = "hidden"
+    selectedSkillInputHidden.name = "skill";
+
+    selectedSkillFeedback.className = "flex-grow-1 card d-flex pt-3 align-items-center"
+    selectedSkillText.className = "text-secondary"
+    selectedSkillText.textContent = selectedSkillName;
+
+    selectedSkillDisplay.className = "input-group my-4";
+
     deleteButton.className = "btn btn-outline-danger custom-light-border ms-3"
     deleteButton.textContent = "❌"
-    selectedSkillDisplay.appendChild(deleteButton);
+
     deleteButton.addEventListener("click", () => {
         selectedSkillDisplay.remove();
     });
-    const selectedSkillsDiv = document.getElementById("selected-skills");
     selectedSkillsDiv.appendChild(selectedSkillDisplay);
+    selectedSkillFeedback.appendChild(selectedSkillText)
+    selectedSkillDisplay.appendChild(selectedSkillFeedback);
+    selectedSkillDisplay.appendChild(selectedSkillInputHidden);
+    selectedSkillDisplay.appendChild(deleteButton);
     skillDropdown.selectedIndex = 0;
 }
