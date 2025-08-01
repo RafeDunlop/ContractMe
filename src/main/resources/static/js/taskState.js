632f8ff3432a304abff81026d27e736b65b5740a
jlc185 <jlc185@uclive.ac.nz>
2025-08-02 10:32:13 +1200

    feat: Updates calendar UI without refreshing the page when task state changes.

--- src/main/resources/static/js/taskState.js
+++ src/main/resources/static/js/taskState.js
@@ -28,9 +28,41 @@ function updateTaskState(taskId, newState, cardSelect) {
 
         if (response.ok && card && newColor) {
             card.style.borderTop = `5px solid ${newColor}`;
+            updateCalendarUI()
         }
 
     }).catch(error => {
-        console.error("Error:", error);
+        console.error("Error: ", error);
     });
+}
+
+/**
+ * Used to update the UI of the calendar without refreshing the page.
+ * Calls changeMonth from calendar.js
+ */
+function updateCalendarUI() {
+    const months = {
+        January: 1,
+        February: 2,
+        March: 3,
+        April: 4,
+        May: 5,
+        June: 6,
+        July: 7,
+        August: 8,
+        September: 9,
+        October: 10,
+        November: 11,
+        December: 12
+    };
+
+    const recordId = document.getElementById("recordId")?.value;
+    const calendarHeader = document.querySelector(`#calendar-${recordId} h1`);
+    const calendarText = calendarHeader.textContent.trim();
+
+    const [monthName, yearString] = calendarText.split(" ");
+    const month = months[monthName];
+    const year = parseInt(yearString);
+
+    changeMonth(recordId, month, year);
 }
