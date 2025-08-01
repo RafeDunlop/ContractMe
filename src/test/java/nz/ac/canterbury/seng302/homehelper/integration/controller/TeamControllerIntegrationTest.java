39f1a276b40168e25099d9a16987fc022fb7be74
Jack <jgu73@uclive.ac.nz>
2025-08-01 15:01:58 +1200

    chore: fix merge conflicts
    Fixed small typos in import statements breaking pipeline

--- src/test/java/nz/ac/canterbury/seng302/homehelper/integration/controller/TeamControllerIntegrationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/homehelper/integration/controller/TeamControllerIntegrationTest.java
@@ -5,7 +5,7 @@ import nz.ac.canterbury.seng302.homehelper.entity.Location;
 import nz.ac.canterbury.seng302.homehelper.entity.RenovationRecord;
 import nz.ac.canterbury.seng302.homehelper.entity.users.User;
 import nz.ac.canterbury.seng302.homehelper.repository.RenovationRecordRepository;
-import nz.ac.canterbury.seng302.homehelper.repository.userReposoitories.UserRepository;
+import nz.ac.canterbury.seng302.homehelper.repository.userRepositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
