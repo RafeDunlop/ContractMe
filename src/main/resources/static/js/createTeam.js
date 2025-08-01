6488a3dc6c08be441db2afb24310606f410d871c
Sean Reitsma <sre114@uclive.ac.nz>
2025-08-01 13:56:42 +1200

    fix: add check to prevent adding empty skill

--- src/main/resources/static/js/createTeam.js
+++ src/main/resources/static/js/createTeam.js
@@ -4,36 +4,38 @@
  */
 function addSkill() {
     const skillDropdown = document.getElementById("skills-select");
-    const selectedSkill = skillDropdown.value;
-    const selectedSkillName = skillDropdown.options[skillDropdown.selectedIndex].dataset.displayname;
-    const selectedSkillsDiv = document.getElementById("selected-skills");
+    if (skillDropdown.selectedIndex !== 0) {
+        const selectedSkill = skillDropdown.value;
+        const selectedSkillName = skillDropdown.options[skillDropdown.selectedIndex].dataset.displayname;
+        const selectedSkillsDiv = document.getElementById("selected-skills");
 
-    const selectedSkillDisplay = document.createElement("div");
-    const selectedSkillFeedback = document.createElement("div");
-    const selectedSkillText = document.createElement("p");
-    const selectedSkillInputHidden = document.createElement("input");
-    const deleteButton = document.createElement("button");
+        const selectedSkillDisplay = document.createElement("div");
+        const selectedSkillFeedback = document.createElement("div");
+        const selectedSkillText = document.createElement("p");
+        const selectedSkillInputHidden = document.createElement("input");
+        const deleteButton = document.createElement("button");
 
-    selectedSkillInputHidden.value = selectedSkill;
-    selectedSkillInputHidden.type = "hidden"
-    selectedSkillInputHidden.name = "skill";
+        selectedSkillInputHidden.value = selectedSkill;
+        selectedSkillInputHidden.type = "hidden"
+        selectedSkillInputHidden.name = "skill";
 
-    selectedSkillFeedback.className = "flex-grow-1 card d-flex pt-3 align-items-center"
-    selectedSkillText.className = "text-secondary"
-    selectedSkillText.textContent = selectedSkillName;
+        selectedSkillFeedback.className = "flex-grow-1 card d-flex pt-3 align-items-center"
+        selectedSkillText.className = "text-secondary"
+        selectedSkillText.textContent = selectedSkillName;
 
-    selectedSkillDisplay.className = "input-group my-4";
+        selectedSkillDisplay.className = "input-group my-4";
 
-    deleteButton.className = "btn btn-outline-danger custom-light-border ms-3"
-    deleteButton.textContent = "❌"
+        deleteButton.className = "btn btn-outline-danger custom-light-border ms-3"
+        deleteButton.textContent = "❌"
 
-    deleteButton.addEventListener("click", () => {
-        selectedSkillDisplay.remove();
-    });
-    selectedSkillsDiv.appendChild(selectedSkillDisplay);
-    selectedSkillFeedback.appendChild(selectedSkillText)
-    selectedSkillDisplay.appendChild(selectedSkillFeedback);
-    selectedSkillDisplay.appendChild(selectedSkillInputHidden);
-    selectedSkillDisplay.appendChild(deleteButton);
-    skillDropdown.selectedIndex = 0;
+        deleteButton.addEventListener("click", () => {
+            selectedSkillDisplay.remove();
+        });
+        selectedSkillsDiv.appendChild(selectedSkillDisplay);
+        selectedSkillFeedback.appendChild(selectedSkillText)
+        selectedSkillDisplay.appendChild(selectedSkillFeedback);
+        selectedSkillDisplay.appendChild(selectedSkillInputHidden);
+        selectedSkillDisplay.appendChild(deleteButton);
+        skillDropdown.selectedIndex = 0;
+    }
 }
