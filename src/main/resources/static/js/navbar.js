6ac0317f9cc8716ff935b46401013e134fcb827a
Jack Guard <jgu73@uclive.ac.nz>
2025-08-03 18:48:24 +1200

    feat: navbar user menu dropdown
    Basic outline for dropdown now functional when on home page

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,10 +1,10 @@
 function displayMinimisedMenu() {}
 
-console.log("script loaded")
 
-function displayUserMenus() {
-    console.log("user menus displayed");
-    document.getElementById("user-menus").classList.toggle("dropDownBox");
+function toggleUserMenus() {
+    console.log("funct loaded")
+
+    document.getElementById("dropdown").classList.toggle("show-dropdown-box");
 }
 
-window.displayUserMenus = displayUserMenus;
+window.toggleUserMenus = toggleUserMenus;
