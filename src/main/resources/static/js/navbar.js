b44c25efa9a8109de1b3249ce144f36d30a4c982
jlc185 <jlc185@uclive.ac.nz>
2025-08-04 16:00:48 +1200

    feat: Add styles.css to all pages.
    Signed-off-by: Jack <jgu73@uclive.ac.nz>

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,28 +1,23 @@
-
-
-
 /**
  * Opens or closes the dropdown menu when the user clicks on the profile image
  */
-function toggleUserMenus() {
-    console.log("funct loaded")
-
-    document.getElementById("dropdown").classList.toggle("show-hidden-element");
-}
-
-
-/**
- * Closes the dropdown menu when the user clicks anywhere on the
- * screen apart from the profile image
- * @param event the MouseEvent tracking where the user clicked
- */
-window.onclick = function closeDropdownMenu(event) {
-    let profileDropdown = document.getElementById("dropdown");
-    let profileImage = document.getElementById("profile-image");
-    if (!profileImage.contains(event.target)) {
-        if (profileDropdown.classList.contains("show-hidden-element")) {
-            profileDropdown.classList.remove('show-hidden-element');
-        }
-    }
-}
-
+// function toggleUserMenus() {
+//     document.getElementById("dropdown").classList.toggle("show-hidden-element");
+// }
+//
+//
+// /**
+//  * Closes the dropdown menu when the user clicks anywhere on the
+//  * screen apart from the profile image
+//  * @param event the MouseEvent tracking where the user clicked
+//  */
+// window.onclick = function closeDropdownMenu(event) {
+//     let profileDropdown = document.getElementById("dropdown");
+//     let profileImage = document.getElementById("profile-image");
+//     if (!profileImage.contains(event.target)) {
+//         if (profileDropdown.classList.contains("show-hidden-element")) {
+//             profileDropdown.classList.remove('show-hidden-element');
+//         }
+//     }
+// }
+//
