89a25e62f9559d3c870cfe75e585c0b0bc2b2b70
Abhisekh Chand <ach331@uclive.ac.nz>
2025-08-04 23:06:20 +1200

    Removed to do

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/TeamController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/TeamController.java
@@ -72,7 +72,6 @@ public class TeamController {
             if (renovationRecord == null || !loggedIn.equals(renovationRecord.getUser()) || !locationService.hasLocation(renovationRecord) || teamsService.teamExists(renovationRecord.getId())) {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
             }
-            //todo check that record doesn't already have a a team! This should be implemented by task "Implement form submission"
             model.addAttribute("teamRequestDTO", teamRequestDTO);
             model.addAttribute("renovationRecord", renovationRecord);
             model.addAttribute("skills", Skill.values());
