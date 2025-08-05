6927632f1669ba137b810f3b3b897961d41df9df
Ryan Hamilton <rha206@uclive.ac.nz>
2025-08-05 20:29:56 +1200

    Test: Integration tests for registerController coordinates.
    - Tests for when a LocationDTO has coordinates, a valid location, and an invalid location.

--- src/test/java/nz/ac/canterbury/seng302/homehelper/integration/controller/RegisterControllerIntegrationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/homehelper/integration/controller/RegisterControllerIntegrationTest.java
@@ -2,6 +2,8 @@ package nz.ac.canterbury.seng302.homehelper.integration.controller;
 
 import jakarta.annotation.PostConstruct;
 import nz.ac.canterbury.seng302.homehelper.controller.RegisterController;
+import nz.ac.canterbury.seng302.homehelper.dto.AddressDTO;
+import nz.ac.canterbury.seng302.homehelper.entity.Location;
 import nz.ac.canterbury.seng302.homehelper.entity.users.Contractor;
 import nz.ac.canterbury.seng302.homehelper.entity.users.User;
 import nz.ac.canterbury.seng302.homehelper.entity.VerificationCode;
@@ -10,17 +12,20 @@ import nz.ac.canterbury.seng302.homehelper.repository.userReposoitories.UserRepo
 import nz.ac.canterbury.seng302.homehelper.repository.VerificationCodeRepository;
 import nz.ac.canterbury.seng302.homehelper.service.EmailService;
 
+import nz.ac.canterbury.seng302.homehelper.service.LocationService;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.support.ArgumentConvertingMethodInvoker;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -62,15 +67,25 @@ public class RegisterControllerIntegrationTest {
     private VerificationCodeRepository verificationCodeRepository;
     @MockBean
     private EmailService emailService;
+    @SpyBean
+    private LocationService locationService;
 
     private User expectedUser;
     private String expectedPassword;
 
     private static Stream<Arguments> streamValidLocationInputsWithCoordinates() {
         return Stream.of(
-                Arguments.of("1 Address", "Suburb", "1111", "Country", "City", 45L, 90L),
-                Arguments.of("1 Address", "", "", "", "", 45L, 90L),
-                Arguments.of("", "", "", "", "", 45L, 90L)
+                Arguments.of("1 Address", "Suburb", "City", "1111", "Country", 45D, 90D),
+                Arguments.of("2 Address", "Suburb", "City", "", "", -45D, -90D),
+                Arguments.of("3 Address", "", "", "", "", 0.001D, -0.001D)
+        );
+    }
+
+    private static Stream<Arguments> streamValidLocationInputsWithoutCoordinates() {
+        return Stream.of(
+                Arguments.of("1 Address", "Suburb", "City", "1111", "Country"),
+                Arguments.of("2 Address", "Suburb", "City", "", ""),
+                Arguments.of("3 Address", "", "", "", "")
         );
     }
 
@@ -78,6 +93,7 @@ public class RegisterControllerIntegrationTest {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         expectedPassword = "Test123!";
         expectedUser = Mockito.spy(new User("Jane", "Doe", "jane@doe.nz", passwordEncoder.encode(expectedPassword)));
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(expectedUser);
     }
 
     /**
@@ -86,7 +102,6 @@ public class RegisterControllerIntegrationTest {
     @PostConstruct
     public void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(registerController).build();
-
     }
 
     /**
@@ -264,8 +279,9 @@ public class RegisterControllerIntegrationTest {
 
     @ParameterizedTest
     @MethodSource("streamValidLocationInputsWithCoordinates")
-    public void submitRegistration_inputValidLocations_return(String address, String suburb, String city, String postcode,
-                                                              String country, Long lat, Long lon) throws Exception {
+    public void submitRegistration_inputValidLocationsWithCoordinates_successfulRegistrationWithAutocompleteCoordinates(String address, String suburb, String city,
+                                                                                                                        String postcode, String country, Double lat,
+                                                                                                                        Double lon) throws Exception {
         createValidUser();
         mockMvc.perform(MockMvcRequestBuilders.post("/register")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
@@ -273,14 +289,116 @@ public class RegisterControllerIntegrationTest {
                         .param("lastName", expectedUser.getLastName())
                         .param("email", expectedUser.getEmail())
                         .param("password", expectedPassword)
+                        .param("confirmPassword", expectedPassword)
                         .param("address_line1", address)
-                        .param("suburb", suburb)
+                        .param("region", suburb)
                         .param("city", city)
                         .param("postcode", postcode)
                         .param("country", country)
-                        .param("lat", Long.toString(lat))
-                        .param("lon", Long.toString(lon)))
+                        .param("lat", Double.toString(lat))
+                        .param("lon", Double.toString(lon)))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(view().name("redirect:/confirm-registration"));
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(userRepository, Mockito.atLeastOnce()).save(userCaptor.capture());
+        User registeredUser = userCaptor.getValue();
+        Location registeredUserLocation = registeredUser.getLocation();
+
+        Assertions.assertEquals(address, registeredUserLocation.getAddress());
+        Assertions.assertEquals(suburb, registeredUserLocation.getSuburb());
+        Assertions.assertEquals(city, registeredUserLocation.getCity());
+        Assertions.assertEquals(postcode, registeredUserLocation.getPostcode());
+        Assertions.assertEquals(country, registeredUserLocation.getCountry());
+        Assertions.assertEquals(lat, registeredUserLocation.getLatitude());
+        Assertions.assertEquals(lon, registeredUserLocation.getLongitude());
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamValidLocationInputsWithoutCoordinates")
+    public void submitRegistration_inputValidLocationsWithoutCoordinates_successfulRegistrationWithGeolocateCoordinates(String address, String suburb, String city,
+                                                                                                                        String postcode, String country) throws Exception {
+        createValidUser();
+
+        // Prevent call to API and instead add coordinates to DTO when injectCoordsViaGeocoding is called.
+        Mockito.doAnswer(invocationOnMock -> {
+            AddressDTO mockAddressDTO = invocationOnMock.getArgument(0);
+            mockAddressDTO.setLat(1D);
+            mockAddressDTO.setLon(1D);
+            return null;
+        }).when(locationService).injectCoordsViaGeocoding(Mockito.any(AddressDTO.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("firstName", expectedUser.getFirstName())
+                        .param("lastName", expectedUser.getLastName())
+                        .param("email", expectedUser.getEmail())
+                        .param("password", expectedPassword)
+                        .param("confirmPassword", expectedPassword)
+                        .param("address_line1", address)
+                        .param("region", suburb)
+                        .param("city", city)
+                        .param("postcode", postcode)
+                        .param("country", country))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(view().name("redirect:/confirm-registration"));
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(userRepository, Mockito.atLeastOnce()).save(userCaptor.capture());
+        User registeredUser = userCaptor.getValue();
+        Location registeredUserLocation = registeredUser.getLocation();
+
+        Assertions.assertEquals(address, registeredUserLocation.getAddress());
+        Assertions.assertEquals(suburb, registeredUserLocation.getSuburb());
+        Assertions.assertEquals(city, registeredUserLocation.getCity());
+        Assertions.assertEquals(postcode, registeredUserLocation.getPostcode());
+        Assertions.assertEquals(country, registeredUserLocation.getCountry());
+        Assertions.assertEquals(1D, registeredUserLocation.getLatitude());
+        Assertions.assertEquals(1D, registeredUserLocation.getLongitude());
+    }
+
+    @Test
+    public void submitRegistration_inputInvalidLocationsWithoutCoordinates_successfulRegistrationWithIpCoordinates() throws Exception {
+        createValidUser();
+        Location expectedLocation = new Location("Fake Place", "Fake Country", "0000", "Fake City", "Fake Suburb");
+
+        // Throw an exception to simulate the API failing to find coordinates for the inputted location.
+        Mockito.doThrow(IllegalArgumentException.class).when(locationService).injectCoordsViaGeocoding(Mockito.any(AddressDTO.class));
+
+        // Prevent call to API and instead add coordinates to DTO when injectCoordsViaIpGeolocation is called.
+        Mockito.doAnswer(invocationOnMock -> {
+            AddressDTO mockAddressDTO = invocationOnMock.getArgument(0);
+            mockAddressDTO.setLat(1D);
+            mockAddressDTO.setLon(1D);
+            return null;
+        }).when(locationService).injectCoordsViaIpGeolocation(Mockito.any(AddressDTO.class), Mockito.anyString());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("firstName", expectedUser.getFirstName())
+                        .param("lastName", expectedUser.getLastName())
+                        .param("email", expectedUser.getEmail())
+                        .param("password", expectedPassword)
+                        .param("confirmPassword", expectedPassword)
+                        .param("address_line1", expectedLocation.getAddress())
+                        .param("region", expectedLocation.getSuburb())
+                        .param("city", expectedLocation.getCity())
+                        .param("postcode", expectedLocation.getPostcode())
+                        .param("country", expectedLocation.getCountry()))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(view().name("redirect:/confirm-registration"));
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(userRepository, Mockito.atLeastOnce()).save(userCaptor.capture());
+        User registeredUser = userCaptor.getValue();
+        Location registeredUserLocation = registeredUser.getLocation();
+
+        Assertions.assertEquals(expectedLocation.getAddress(), registeredUserLocation.getAddress());
+        Assertions.assertEquals(expectedLocation.getSuburb(), registeredUserLocation.getSuburb());
+        Assertions.assertEquals(expectedLocation.getCity(), registeredUserLocation.getCity());
+        Assertions.assertEquals(expectedLocation.getPostcode(), registeredUserLocation.getPostcode());
+        Assertions.assertEquals(expectedLocation.getCountry(), registeredUserLocation.getCountry());
+        Assertions.assertEquals(1D, registeredUserLocation.getLatitude());
+        Assertions.assertEquals(1D, registeredUserLocation.getLongitude());
     }
 }
