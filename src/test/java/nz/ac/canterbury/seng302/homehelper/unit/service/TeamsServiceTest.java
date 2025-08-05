bdbd00dc8f7f24b62bd28eae71b6d27a04191340
Abhisekh Chand <ach331@uclive.ac.nz>
2025-08-06 00:39:24 +1200

    Fix: Failing pipeline fixed.

--- src/test/java/nz/ac/canterbury/seng302/homehelper/unit/service/TeamsServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/homehelper/unit/service/TeamsServiceTest.java
@@ -10,13 +10,12 @@ import nz.ac.canterbury.seng302.homehelper.service.TeamsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 @ExtendWith(MockitoExtension.class)
 public class TeamsServiceTest{
 
-    @MockBean
+    @Mock
     private TeamsRepository teamsRepository;
     private TeamsService teamsService;
 
