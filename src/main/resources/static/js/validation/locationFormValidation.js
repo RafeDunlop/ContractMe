3e494e93da7c53f0b75658443b776e83c38c1a69
Mason Ott <mot30@uclive.ac.nz>
2025-08-08 17:24:47 +1200

    Fix js regex expression to allow empty string

--- src/main/resources/static/js/validation/locationFormValidation.js
+++ src/main/resources/static/js/validation/locationFormValidation.js
@@ -1,10 +1,8 @@
 const addressPattern = /^[\p{L}\-'\d\s.\/]*$/u;
 const suburbPattern = /^[\p{L}\d\-'\s]*$/u;
 const cityPattern = /^[\p{L}\-'\s]*$/u;
-const postcodePattern = /^(?:\p{L}|\p{N})+(?: (?:\p{L}|\p{N})+)?$/u;
-const countryPattern = /^(?:\p{L}|[-'])+(?: (?:\p{L}|[-'])+)?$/u;
-
-
+const postcodePattern = /^(?:(?:\p{L}|\p{N})+(?: (?:\p{L}|\p{N})+)?)?$/u;
+const countryPattern = /^(?:(?:\p{L}|[-'])+(?: (?:\p{L}|[-'])+)?)?$/u;
 
 let addressField = document.getElementById("address");
 let addressFrontendError = document.getElementById("address-frontend-error");
