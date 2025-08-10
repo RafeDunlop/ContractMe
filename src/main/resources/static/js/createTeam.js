8c577eed0569fd54c00c6cbf8c7a767385117543
jlc185 <jlc185@uclive.ac.nz>
2025-08-10 13:47:16 +1200

    feat: changes requested in merge request

--- src/main/resources/static/js/createTeam.js
+++ src/main/resources/static/js/createTeam.js
@@ -46,35 +46,3 @@ function addSkill() {
     }
 }
 
-/**
- * Controls the enabled state of the "Add" button based on skill selection.
- */
-document.getElementById('skills-select').addEventListener('change', function() {
-    const addButton = document.getElementById('addSkillButton');
-    addButton.disabled = this.selectedIndex === 0;
-});
-
-/** Used for front end validation for checking amount of skills selected */
-function getSkillCount() {
-    return document.querySelectorAll('#selected-skills input[name="skills"]').length;
-}
-
-/** Update error message labels*/
-function updateErrorMessageLabels() {
-    const count = getSkillCount();
-
-    let sizeFrontendError = document.getElementById("skill-frontend-error");
-    let sizeFrontendErrorMessage = document.getElementById("skill-frontend-error-message");
-
-    if (count === 0) {
-        sizeFrontendErrorMessage.textContent = "Your team request must have at least one role.";
-        sizeFrontendError.hidden = false;
-    } else if (count > 5) {
-        sizeFrontendErrorMessage.textContent = "Your team request cannot have more than 5 roles.";
-        sizeFrontendError.hidden = false;
-    } else {
-        sizeFrontendErrorMessage.textContent = "";
-        sizeFrontendError.hidden = true;
-    }
-    return count;
-}
