182ed597d2892b24650686722707a8f14ecb9456
jlc185 <jlc185@uclive.ac.nz>
2025-08-18 11:07:29 +1200

    test: fix integration and cucumber tests, due to contractor lookup throwing exception instead of null.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/service/ContractorService.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/service/ContractorService.java
@@ -3,7 +3,6 @@ package nz.ac.canterbury.seng302.homehelper.service;
 import jakarta.persistence.EntityNotFoundException;
 import nz.ac.canterbury.seng302.homehelper.dto.AddressDTO;
 import nz.ac.canterbury.seng302.homehelper.dto.UserRegisterDTO;
-import nz.ac.canterbury.seng302.homehelper.entity.Team;
 import nz.ac.canterbury.seng302.homehelper.entity.users.Contractor;
 import nz.ac.canterbury.seng302.homehelper.repository.userRepositories.ContractorRepository;
 import nz.ac.canterbury.seng302.homehelper.util.MapUtil;
@@ -33,9 +32,10 @@ public class ContractorService {
     private final LocationService locationService;
 
     /**
-     * Constructor for the service and links the repository and validator to the
-     * service.
+     * Constructor for the service and links the repository and validator to the service.
      * @param contractorRepository ContractorRepository for getting and updating contractor details
+     * @param contractorValidation validation for contractor related inputs
+     * @param locationService the location service for setting locations
      */
     @Autowired
     public ContractorService(ContractorRepository contractorRepository, ContractorValidation contractorValidation, LocationService locationService) {
@@ -99,6 +99,16 @@ public class ContractorService {
      */
     public Contractor getContractorById(Long id) {
         return contractorRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Contractor: " + id + " not found"));
+    }
+
+    /**
+     * Retrieves a contractor by their identifier.
+     *
+     * @param id the unique identifier of the contractor to be retrieved
+     * @return the contractor associated with the given ID, or null if no contractor is found
+     */
+    public Contractor getContractorByIdElseNull(Long id) {
+        return contractorRepository.findById(id).orElse(null);
     }
 
 }
