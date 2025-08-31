ce369e22b5808eb23cf042b2b23ff378146111be
Abhisekh Chand <ach331@uclive.ac.nz>
2025-08-31 15:46:53 +1200

    Feature : View request on navbar - View request is only shown on navbar if the user is a contractor which detected by a session attribute.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/MainController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/MainController.java
@@ -1,4 +1,8 @@
 package nz.ac.canterbury.seng302.homehelper.controller;
+import jakarta.servlet.http.HttpSession;
+import nz.ac.canterbury.seng302.homehelper.entity.users.Contractor;
+import nz.ac.canterbury.seng302.homehelper.entity.users.User;
+import nz.ac.canterbury.seng302.homehelper.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,14 +18,22 @@ public class MainController {
 
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
+    private final LoginService loginService;
+
+    public MainController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     /**
      * Handler for a get request to the main page.
      *
      * @return mainTemplate
      */
     @GetMapping("/main")
-    public String login(Model model) {
+    public String login(Model model, HttpSession session) {
         logger.info("GET /Main");
+        User user = loginService.getUserByEmail();
+        session.setAttribute("isContractor",user instanceof Contractor contractor);
         return "mainTemplate";
     }
 }
