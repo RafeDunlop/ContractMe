bee1f312d2dc334754348b032fda10be5b756cf4
rdu46 <rdu46@uclive.ac.nz>
2025-09-11 17:03:06 +1200

    refactored coordinates into a DTO

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/MapController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/MapController.java
@@ -1,46 +1,40 @@
 package nz.ac.canterbury.seng302.homehelper.controller;
 
+import nz.ac.canterbury.seng302.homehelper.dto.CoordinateRectangle;
 import nz.ac.canterbury.seng302.homehelper.dto.MappedRenovation;
-import nz.ac.canterbury.seng302.homehelper.entity.users.User;
-import nz.ac.canterbury.seng302.homehelper.service.LoginService;
+import nz.ac.canterbury.seng302.homehelper.service.MapService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
-import java.util.List;
 
 @Controller
 @RequestMapping("/map")
 public class MapController {
 
-    private final LoginService loginService;
+    private final MapService mapService;
 
     @Autowired
-    public MapController(LoginService loginService) {
-        this.loginService = loginService;
+    public MapController(MapService mapService) {
+        this.mapService = mapService;
     }
 
     /**
-     * Gets renovations located within a box described by the co-ordinates specified
+     * Gets renovations located within a box described by the co-ordinates specified.
+     * Specify bounding coords via bounds.[min/max][Lat/Lon]
      * @param withPublic Whether to include public renovations not owned by the logged-in user within the range.
      *                   Default true
-     * @param minLat The minimum latitude to be fetched
-     * @param minLon The minimum longitude to be fetched
-     * @param maxLat The maximum latitude to be fetched
-     * @param maxLon The maximum longitude to be fetched
+     * @param coordinateRectangle Contains the longitude and latitudes values which determine the bounding rectangle
      * @return a {@link Collection} of {@link MappedRenovation} DTO objects which contain the minimal requisite details
      */
     @GetMapping("/renovations")
     public Collection<MappedRenovation> getByLocationInBounds(
             @RequestParam(required = false, defaultValue = "true") boolean withPublic,
-            @RequestParam double minLat,
-            @RequestParam double minLon,
-            @RequestParam double maxLat,
-            @RequestParam double maxLon) {
-        User loggedIn = loginService .getUserByEmail();
-        return List.of();
+            @ModelAttribute("bounds") CoordinateRectangle coordinateRectangle) {
+        return mapService.getRenovationsInBounds(coordinateRectangle, withPublic);
     }
 }
---
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/dto/CoordinateRectangle.java
@@ -0,0 +1,41 @@
+package nz.ac.canterbury.seng302.homehelper.dto;
+
+public class CoordinateRectangle {
+
+    private double minLat;
+    private double minLon;
+    private double maxLat;
+    private double maxLon;
+
+    public double getMinLat() {
+        return minLat;
+    }
+
+    public void setMinLat(double minLat) {
+        this.minLat = minLat;
+    }
+
+    public double getMinLon() {
+        return minLon;
+    }
+
+    public void setMinLon(double minLon) {
+        this.minLon = minLon;
+    }
+
+    public double getMaxLat() {
+        return maxLat;
+    }
+
+    public void setMaxLat(double maxLat) {
+        this.maxLat = maxLat;
+    }
+
+    public double getMaxLon() {
+        return maxLon;
+    }
+
+    public void setMaxLon(double maxLon) {
+        this.maxLon = maxLon;
+    }
+}
