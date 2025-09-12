73eff08601d5677de1268034b2cdb192bfa51c99
Mason Ott <mot30@uclive.ac.nz>
2025-09-12 14:50:22 +1200

    Remove add skill button and update dropdown to automatically add role. Implement consistent remove button to role card. Update role card UI to be more clean and understandable. Improve responsiveness.

--- src/main/resources/static/js/createTeam.js
+++ src/main/resources/static/js/createTeam.js
@@ -1,116 +1,73 @@
-function updateAddRoleVisibility() {
-    const skillsGridRow = document.querySelector(".container.text-center .row");
-    const addRoleCard = document.getElementById("add-role-card");
-    const skillCards = skillsGridRow.querySelectorAll(".skill-card");
-    const count = skillCards.length;
-    if (count >= 5) {
-        if (addRoleCard) {
-            addRoleCard.style.display = "none";
-        }
-    } else {
-        if (addRoleCard) {
-            addRoleCard.style.display = "";
-        }
-    }
-}
-
 function addSkill() {
     const skillDropdown = document.getElementById("skills-select");
     if (skillDropdown.selectedIndex !== 0) {
-        const selectedSkill = skillDropdown.value;
         const selectedSkillName = skillDropdown.options[skillDropdown.selectedIndex].dataset.displayname;
-
-        const selectedSkillsDiv = document.getElementById("selected-skills");
+        const selectedSkill = skillDropdown.value;
         const skillsGridRow = document.querySelector(".container.text-center .row");
 
         const existingSkillCards = skillsGridRow.querySelectorAll(".skill-card");
         if (existingSkillCards.length >= 5) {
+            displayTooManyRolesError();
+            skillDropdown.selectedIndex = 0;
             return;
         }
 
-        const selectedSkillDisplay = document.createElement("div");
-        const selectedSkillFeedback = document.createElement("div");
-        const selectedSkillText = document.createElement("p");
-        const selectedSkillInputHidden = document.createElement("input");
-        const deleteButton = document.createElement("button");
-
-        selectedSkillInputHidden.value = selectedSkill;
-        selectedSkillInputHidden.type = "hidden";
-        selectedSkillInputHidden.name = "skills";
-
-        selectedSkillFeedback.className = "d-flex justify-content-between align-items-start";
-        selectedSkillText.className = "w-100 text-secondary";
-        selectedSkillText.textContent = selectedSkillName;
-
-        selectedSkillDisplay.className = "list-group-item p-3 mb-3 shadow-sm rounded bg-white position-relative";
-
-        deleteButton.className = "btn btn-outline-danger custom-light-border ms-3";
-        deleteButton.textContent = "❌";
-
-        let skillCard;
-
-        deleteButton.addEventListener("click", () => {
-            selectedSkillDisplay.remove();
-            if (skillCard) {
-                skillCard.remove();
-            }
-            updateErrorMessageLabels();
-            updateAddRoleVisibility();
-        });
-
-        selectedSkillsDiv.appendChild(selectedSkillDisplay);
-        selectedSkillFeedback.appendChild(selectedSkillText);
-        selectedSkillDisplay.appendChild(selectedSkillFeedback);
-        selectedSkillDisplay.appendChild(selectedSkillInputHidden);
-        selectedSkillFeedback.appendChild(deleteButton);
-
         const skillCol = document.createElement("div");
-        skillCol.className = "col skill-card";
+        skillCol.className = "col skill-card d-flex justify-content-center";
 
         const skillCardContainer = document.createElement("div");
-        skillCardContainer.className = "skill-card-container card container border-5 border-dark-subtle align-items-center";
-        skillCardContainer.style.paddingTop = "7vh";
+        skillCardContainer.className = "skill-card-container card container border-5 border-black align-items-center";
+        skillCardContainer.style.paddingTop = "6vh";
         skillCardContainer.style.paddingBottom = "4vh";
         skillCardContainer.style.maxWidth = "14vw";
         skillCardContainer.style.height = "35vh";
+        skillCardContainer.style.maxHeight = "400px";
         skillCardContainer.style.margin = "5vh 5% 4vh";
         skillCardContainer.style.position = "relative";
 
-// Image and text
+        // Image and text
         const skillImg = document.createElement("img");
         skillImg.src = "icons/profile-icon.svg";
         skillImg.alt = "default-icon";
         skillImg.style.width = "75%";
-        skillImg.style.marginBottom = "1vh";
-        skillImg.style.marginTop = "1vh";
+        skillImg.style.marginBottom = "0";
+        skillImg.style.marginTop = "0";
 
-        const skillTitle = document.createElement("h6");
+        const skillTitle = document.createElement("h5");
         skillTitle.textContent = selectedSkillName;
+        skillTitle.style.marginBottom = "1vh";
+        skillTitle.className = "selected-skill-name";
 
-// Delete button
-        const deleteSkillBtn = document.createElement("div");
-        deleteSkillBtn.className = "delete-skill-btn";
-        deleteSkillBtn.textContent = "X";
+        const nameTitle = document.createElement("h5");
+        nameTitle.textContent = "Role:";
+        nameTitle.style.marginBottom = "1vh";
+
+        // Delete button
+        const deleteSkillBtn = document.createElement("button");
+        deleteSkillBtn.className = "delete-skill-btn btn btn-outline-danger custom-light-border ms-3";
+        deleteSkillBtn.textContent = "❌";
+
+        const hiddenInput = document.createElement("input");
+        hiddenInput.type = "hidden";
+        hiddenInput.name = "skills";
+        hiddenInput.value = selectedSkill;
+
+        skillCol.appendChild(hiddenInput);
 
         deleteSkillBtn.addEventListener("click", () => {
             skillCol.remove();
-            selectedSkillDisplay.remove();
             updateErrorMessageLabels();
-            updateAddRoleVisibility();
         });
 
-// Append elements
         skillCardContainer.appendChild(deleteSkillBtn);
         skillCardContainer.appendChild(skillImg);
+        skillCardContainer.appendChild(nameTitle);
         skillCardContainer.appendChild(skillTitle);
 
         skillCol.appendChild(skillCardContainer);
         skillsGridRow.appendChild(skillCol);
-
-
+        document.getElementById("create-team-form").appendChild(hiddenInput);
         skillDropdown.selectedIndex = 0;
-
-        updateAddRoleVisibility();
         updateErrorMessageLabels();
     }
 }
--- src/main/resources/static/js/validation/createTeamValidation.js
+++ src/main/resources/static/js/validation/createTeamValidation.js
@@ -1,14 +1,13 @@
-/**
- * Controls the enabled state of the "Add" button based on skill selection.
- */
-document.getElementById('skills-select').addEventListener('change', function() {
-    const addButton = document.getElementById('addSkillButton');
-    addButton.disabled = this.selectedIndex === 0;
-});
-
 /** Used for front end validation for checking amount of skills selected */
 function getSkillCount() {
-    return document.querySelectorAll('#selected-skills input[name="skills"]').length;
+    return document.querySelectorAll('.skill-card-container').length;
+}
+
+function displayTooManyRolesError() {
+    let sizeFrontendError = document.getElementById("skill-frontend-error");
+    let sizeFrontendErrorMessage = document.getElementById("skill-frontend-error-message");
+    sizeFrontendErrorMessage.textContent = "Your team request cannot have more than 5 roles.";
+    sizeFrontendError.hidden = false;
 }
 
 /** Update error message labels*/
@@ -21,10 +20,6 @@ function updateErrorMessageLabels() {
 
     if (count === 0) {
         sizeFrontendErrorMessage.textContent = "Your team request must have at least one role.";
-        sizeFrontendError.hidden = false;
-        submitButton.disabled = true;
-    } else if (count > 5) {
-        sizeFrontendErrorMessage.textContent = "Your team request cannot have more than 5 roles.";
         sizeFrontendError.hidden = false;
         submitButton.disabled = true;
     } else {
