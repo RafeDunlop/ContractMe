dffd9d38bbe4ea99803fb9922534293b0a3184ad
Ryan Hamilton <rha206@uclive.ac.nz>
2025-09-17 12:04:40 +1200

    Spike: Final version of the example code for Leaflet + OpenStreetMaps.

--- src/main/resources/static/js/homeMap.js
+++ src/main/resources/static/js/homeMap.js
@@ -1,31 +1,20 @@
-const locationResponse = await fetch("location/localisation", {method: "GET"});
-const renovationResponse = await fetch("map/renovations", {method: "GET"});
-
 const userRenovation = L.icon({
-    iconUrl: "map/marker/user-renovation",
+    iconUrl: "/images/markers/user-renovation.png",
     iconSize: [32, 32]
 })
 
 const publicRenovation = L.icon({
-    iconUrl: "map/marker/public-renovation",
+    iconUrl: "/images/markers/public-renovation.png",
     iconSize: [32, 32]
 })
 
-if (locationResponse.ok) {
-    const locationData = await locationResponse.json();
-    const { latitude, longitude } = locationData.location;
-    const map = L.map('map').setView([latitude, longitude], 11);
+const map = L.map('map').setView([-43.52460, 172.57710], 11);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        minZoom: 3,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    minZoom: 3,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-    const renovationData = await renovationResponse.json();
-    renovationData.forEach(renovation => {
-        L.marker([renovation.location.latitude, renovation.location.longitude], {icon: userRenovation}).addTo(map);
-    })
-} else {
-    console.log("Not found.")
-}
+L.marker([-43.52460, 172.57710], {icon: userRenovation}).addTo(map);
+L.marker([-43.53333, 172.63333], {icon: publicRenovation}).addTo(map);
