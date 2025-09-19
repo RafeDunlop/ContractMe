6477148c19bb849548676ab71c516f2b36fce00d
Ryan <rha206@uclive.ac.nz>
2025-09-19 16:20:58 +1200

    feat: tasks not loading, due to pagination issues.
    Signed-off-by: Ryan <rha206@uclive.ac.nz>

--- src/main/resources/static/js/viewRenovationTabBar.js
+++ src/main/resources/static/js/viewRenovationTabBar.js
@@ -8,7 +8,14 @@ document.querySelectorAll('.nav-link').forEach(btn => {
     btn.addEventListener('click', function () {
         document.querySelector('.nav-link.active')?.classList.remove('active');
         this.classList.add('active');
-        document.querySelector('.renovation-content')?.style.display('none');
+        document.querySelectorAll('.renovation-content').forEach(content => {
+            content.style.display = 'none';
+        });
         document.getElementById(contentMap.get(btn.id)).style.removeProperty('display');
+
+        if (btn.id === "tasks-tab-item") {
+            let id = document.getElementById("recordId").value;
+            updateLayout("cards", id, true);
+        }
     });
 });
