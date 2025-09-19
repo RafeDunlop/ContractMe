04ebcc2dcea58904bd8f81f9cba5893ed2ba2886
rha206 <rha206@uclive.ac.nz>
2025-09-19 15:08:37 +1200

    feat: Change displayed content based on active tab button.
    Signed-off-by: Jake Connolly <jlc185@uclive.ac.nz>

--- src/main/resources/static/js/viewRenovationTabBar.js
+++ src/main/resources/static/js/viewRenovationTabBar.js
@@ -1,12 +1,14 @@
-const detailsTabItem = document.getElementById("details-tab-item");
+const contentMap = new Map([
+    ["details-tab-item", "details-content"],
+    ["tasks-tab-item", "tasks-content"],
+    ["calendar-tab-item", "calendar-content"]
+]);
 
 document.querySelectorAll('.nav-link').forEach(btn => {
     btn.addEventListener('click', function () {
-        console.log(this);
-        if (this.classList.contains('disabled')) {
-            return;
-        }
         document.querySelector('.nav-link.active')?.classList.remove('active');
         this.classList.add('active');
+        document.querySelector('.renovation-content')?.style.display('none');
+        document.getElementById(contentMap.get(btn.id)).style.removeProperty('display');
     });
 });
