3177d2dbd8fca76bce7756e1bca01cef79fa09d8
Jack <jgu73@uclive.ac.nz>
2025-09-20 15:02:56 +1200

    feat: get request for renovations on map
    Wrote get request to call map endpoint
    
    Passed the four coordinates to the controller as a pathvariable, which is then converted to a CoordsRectangle in the MapService.
    
    Signed-off-by: Jack <jgu73@uclive.ac.nz>

--- src/main/resources/static/js/homeMap.js
+++ src/main/resources/static/js/homeMap.js
@@ -1,3 +1,5 @@
+
+
 const userRenovation = L.icon({
     iconUrl: "/images/markers/user-renovation.png",
     iconSize: [32, 32]
@@ -8,13 +10,41 @@ const publicRenovation = L.icon({
     iconSize: [32, 32]
 })
 
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
+
 const map = L.map('map').setView([-43.52460, 172.57710], 11);
+const bounds = map.getBounds();
+const southwest = bounds.getSouthWest();
+const northeast = bounds.getNorthEast();
+const rawCoordinates = [southwest.lat, southwest.lng, northeast.lat, northeast.lng]
+console.log(rawCoordinates.join(","))
+
+
+const renovationResponse = await fetch(`map/renovations/${encodeURIComponent(rawCoordinates.join(","))}`, {method: "GET"});
+
+
+//const debouncedHandleMapChange = debounce(handleMapChange, 700)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     minZoom: 3,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(map);
+
+for (const eventName of ["click", "moveend"]) {
+    map.on(eventName, debouncedHandleMapChange)
+    const renovationData = await renovationResponse.json();
+    renovationData.forEach(renovation => {
+        L.marker([renovation.location.latitude, renovation.location.longitude], {icon: userRenovation}).addTo(map);
+    })
+}
 
 L.marker([-43.52460, 172.57710], {icon: userRenovation}).addTo(map);
 L.marker([-43.53333, 172.63333], {icon: publicRenovation}).addTo(map);
