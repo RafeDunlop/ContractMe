5aebd704c2db368cb96e87272f8f5250d7afba64
rha206 <rha206@uclive.ac.nz>
2025-09-22 18:01:41 +1200

    Implement controller to sent contractor data based on team for displaying location. Create dto for sending this data. Add authentication for controller.

--- src/main/java/nz/ac/canterbury/seng302/homehelper/controller/MapController.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/controller/MapController.java
@@ -2,21 +2,31 @@ package nz.ac.canterbury.seng302.homehelper.controller;
 
 import nz.ac.canterbury.seng302.homehelper.dto.CoordinateRectangle;
 import nz.ac.canterbury.seng302.homehelper.dto.MappedRenovation;
-import nz.ac.canterbury.seng302.homehelper.service.MapService;
+import nz.ac.canterbury.seng302.homehelper.entity.RenovationRecord;
+import nz.ac.canterbury.seng302.homehelper.entity.Team;
+import nz.ac.canterbury.seng302.homehelper.entity.users.Contractor;
+import nz.ac.canterbury.seng302.homehelper.entity.users.User;
+import nz.ac.canterbury.seng302.homehelper.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/map")
 public class MapController {
 
     private final MapService mapService;
+    private final TeamsService teamsService;
+    private final LoginService loginService;
 
     @Autowired
-    public MapController(MapService mapService) {
+    public MapController(MapService mapService, TeamsService teamsService, LoginService loginService) {
         this.mapService = mapService;
+        this.teamsService = teamsService;
+        this.loginService = loginService;
     }
 
     /**
@@ -33,5 +43,21 @@ public class MapController {
             @PathVariable String rawCoordinates) {
         CoordinateRectangle coordinateRectangle = mapService.createCoordinateRectangle(rawCoordinates);
         return mapService.getRenovationsInBounds(coordinateRectangle, withPublic);
+    }
+
+    @GetMapping("/contractors")
+    public Collection<Contractor> getContractorByRenovationId(
+            @RequestParam String id) {
+        long teamId = Long.parseLong(id);
+        User user = loginService.getUserByEmail();
+        Team team = teamsService.getTeamById(teamId);
+        RenovationRecord renovationRecord = team.getRenovationRecord();
+        Boolean isInTeam = teamsService.checkViewRenovationAccess(renovationRecord, user);
+//        if (isInTeam) {
+            Map<Long, Contractor> contractorMap = teamsService.getContractorsByTeamId(teamId);
+            return new ArrayList<>(contractorMap.values());
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//        }
     }
 }
