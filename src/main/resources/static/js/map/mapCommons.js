edadb4a30d8769941410dcf3d8825e9931f1de86
rdu46 <rdu46@uclive.ac.nz>
2025-09-22 17:55:52 +1200

    chore: merged necessary functionality from U2007/toggle-button into U2013/plot-contractor-locations
    homeMap.js, mapCommons.js and mainTemplate.html
    
    Signed-off-by: Jack <jgu73@uclive.ac.nz>

---
+++ src/main/resources/static/js/map/mapCommons.js
@@ -0,0 +1,25 @@
+export function fetchRenovationMappings(bounds, showPrivateOnly) {
+
+    const withPublic = !showPrivateOnly
+    const params = new URLSearchParams();
+    params.set("withPublic", withPublic.toString());
+    params.set("minLat", bounds.minLat);
+    params.set("minLon", bounds.minLon);
+    params.set("maxLat", bounds.maxLat);
+    params.set("maxLon", bounds.maxLon);
+    return fetch(`map/renovations?${params}`)
+}
+
+/**
+ * Delays all UI updates by the specified duration
+ * @param func the function that triggers the UI update
+ * @param wait the duration of delay
+ * @returns {(function(...[*]): void)|*}
+ */
+export function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
