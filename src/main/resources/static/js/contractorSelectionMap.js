a457a80d9d27613009e81962c32c641eac484fa6
Abhisekh Chand <ach331@uclive.ac.nz>
2025-09-24 12:24:35 +1200

    Feature: Fixed Modal containing map. - fixed issues where the map was not loading properly on the modal. - restyled the modal completely for better visual appeal using bootstrap. - created a new function in js which handles the map creation and resizing.

--- src/main/resources/static/js/contractorSelectionMap.js
+++ src/main/resources/static/js/contractorSelectionMap.js
@@ -1,10 +1,43 @@
-const overlay = document.getElementById("overlay");
+let mapInstance;
 
-function selectContractorToInvite(event) {
-    overlay.style.display = 'block';
-    console.log("did function")
+const locationResponse = await fetch("location/localisation", {method: "GET"});
+
+/* Latitude and longitude of Christchurch. */
+let latitude = -43.52460;
+let longitude =  172.57710;
+
+if (locationResponse.ok) {
+  const locationData = await locationResponse.json();
+  if (locationData.location) {
+    latitude = locationData.location.latitude;
+    longitude = locationData.location.longitude;
+  }
 }
 
-function hideMap() {
-    overlay.style.display = "none";
+
+export function selectContractorToInvite(event) {
+  const modalEl = document.getElementById('mapModal');
+  const modal = new bootstrap.Modal(modalEl);
+  modal.show();
+
+  modalEl.addEventListener('shown.bs.modal', () => {
+    if (!mapInstance) {
+      mapInstance = L.map('mapModalMap').setView([latitude, longitude], 11);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        minZoom: 3,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(mapInstance);
+
+
+    } else {
+      mapInstance.invalidateSize();
+      mapInstance.setView([latitude, longitude], 11);
+    }
+  }, { once: true });
 }
+
+window.selectContractorToInvite = selectContractorToInvite;
+
+
+
