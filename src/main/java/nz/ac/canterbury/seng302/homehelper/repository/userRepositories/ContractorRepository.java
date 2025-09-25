36646408c2a14ff7b96b9f7a964d796c6e9873ff
Sean Reitsma <sean@doubleflatwhite.nz>
2025-09-25 17:55:40 +1200

    docs: add missing JavaDoc to repository method

--- src/main/java/nz/ac/canterbury/seng302/homehelper/repository/userRepositories/ContractorRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/homehelper/repository/userRepositories/ContractorRepository.java
@@ -56,6 +56,15 @@ public interface ContractorRepository extends UserBaseRepository<Contractor> {
             @Param("excludedIds") java.util.Set<Long> excludedIds
     );
 
+    /**
+     * Finds all contractors with the given skill within max distance from the given latitude and longitude coordinates.
+     * This is used to find eligible contractors when selecting contractors via the map.
+     * @param skill the required skill to match against the contractors
+     * @param lat the latitude of the specified location, i.e. the renovation location
+     * @param lon the longitude of the specified location
+     * @param maxDistance the maximum distance contractors must be within (inclusive)
+     * @return the list of matching contractors
+     */
     @Query(value = """
                 SELECT c.*
                 FROM user_details c
